68296a9a64ff9ee58724c2d53c812afbab044962
ReginaldOfori <97319184+ReginaldOfori@users.noreply.github.com>
2023-02-20 16:45:21 -0500

    Reginald Documentation initial

--- src/components/Misc/Buttons.js
+++ src/components/Misc/Buttons.js
@@ -22,3 +22,27 @@ export const NavBarButton = ({ handleAction,page,text,idx}) => {
 };
 
 export default NavBarButton;
+
+/* Exports a React functional component called NavBarButton using the default 
+export syntax. 
+It imports several libraries and modules at the beginning of the code, 
+including styled, React, Button from @mui/material, Nav from react-bootstrap,
+ Link from react-router-dom, useAuth and onAuthStateChanged from reactfire, 
+ and uuidv4 from @firebase/util.
+
+The NavBarButton component takes four props, namely handleAction, page, 
+text, and idx, and returns a styled Material UI button with a link to a 
+specified page.
+
+The handleAction prop is a function to be executed when the button is 
+clicked, page is a string indicating the path to the page the button will link 
+to, text is the text to be displayed inside the button, and idx is the id of 
+the button's link.
+
+Inside the NavBarButton component, the useAuth hook from reactfire is called to
+ get the current user's authentication status.
+ 
+ The useState hook is also called to set the profile state to null.
+
+Current Issue: The useEffect hook is currently empty, which means that it doesn't
+ perform any side effects when the component is mounted or updated.*/
