1ac62d6239793a5336b7ca9ee4cb81d55313285b
Cyril Thomas <cyrilkthomasest.1991@gmail.com>
2023-03-29 19:10:10 -0400

    fixes

--- src/components/Filter/FilteredProperties.js
+++ src/components/Filter/FilteredProperties.js
@@ -1,37 +1,71 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import FilterDropdown from "./FilterDropdown";
 import { getFilteredProperties } from "./firebase";
+import {
+Typography,
+Container,
+Box,
+Card,
+CardHeader,
+CardContent,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+root: {
+display: "flex",
+flexDirection: "column",
+alignItems: "center",
+marginTop: theme.spacing(4),
+},
+card: {
+marginTop: theme.spacing(2),
+maxWidth: 600,
+},
+}));
 
 const PropertyListings = () => {
-  const [listings, setListings] = useState([]);
+const classes = useStyles();
+const [listings, setListings] = useState([]);
 
-  const handleFilterChange = async (minPrice, maxPrice, bedrooms, bathrooms) => {
-    const filteredListings = await getFilteredProperties(
-      minPrice,
-      maxPrice,
-      bedrooms,
-      bathrooms
-    );
+const handleFilterChange = async (minPrice, maxPrice, bedrooms, bathrooms) => {
+const filteredListings = await getFilteredProperties(
+minPrice,
+maxPrice,
+bedrooms,
+bathrooms
+);
 
-    setListings(filteredListings);
-  };
+setListings(filteredListings);
+};
 
-  return (
-    <>
-      <FilterDropdown onFilterChange={handleFilterChange} />
-      <div>
-        {listings.map((listing) => (
-          <div key={listing.id}>
-            <h2>{listing.title}</h2>
-            <p>{listing.description}</p>
-            <p>{listing.price}</p>
-            <p>{listing.bedrooms} Bedrooms</p>
-            <p>{listing.bathrooms} Bathrooms</p>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+return (
+<Container className={classes.root}>
+<FilterDropdown onFilterChange={handleFilterChange} />
+<Box mt={2}>
+{listings.map((listing) => (
+<Card key={listing.id} className={classes.card}>
+<CardHeader title={listing.title} />
+<CardContent>
+<Typography variant="body1" component="p">
+{listing.description}
+</Typography>
+<Typography variant="body2" color="textSecondary" component="p">
+{listing.price}
+</Typography>
+<Typography variant="body2" color="textSecondary" component="p">
+{listing.bedrooms} Bedrooms, {listing.bathrooms} Bathrooms
+</Typography>
+</CardContent>
+</Card>
+))}
+</Box>
+</Container>
+);
 };
 
 export default PropertyListings;
+
+
+
+
