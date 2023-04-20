906efcd7be5593b4f380c7e316c5c3878896c55d
= <dcnation1000000@gmail.com>
2023-04-20 14:06:26 -0400

    create edit listing functionality

--- src/pages/Admin/editListing.js
+++ src/pages/Admin/editListing.js
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Box, Button, Typography, TextField } from "@mui/material";
 import { Card, CardContent, CardMedia } from "@mui/material";
-import { collection, doc, updateDoc, getDoc } from "firebase/firestore"
+import { collection, doc, updateDoc, query, getDocs } from "firebase/firestore"
 import { useFirestore } from "reactfire";
 
-const EditListing = ({ listingId }) => {
+const EditListing = () => {
+  const [listings, setListings] = useState([]);
+  const [selectedListing, setSelectedListing] = useState(null);
   const [zip, setZip] = useState('')
   const [street, setStreet] = useState('')
   const [state, setState] = useState('')
@@ -16,64 +18,82 @@ const EditListing = ({ listingId }) => {
   const [image1, setImage1] = useState('')
   const [image2, setImage2] = useState('')
   const [image3, setImage3] = useState('')
-  const [listing, setListing] = useState(null)
   const firestore = useFirestore()
-
-  useEffect(() => {
-    async function fetchListing() {
-      if (listingId) {
-        const listingRef = doc(collection(firestore, "listings", "forSale", "properties"), listingId)
-        const listingDoc = await getDoc(listingRef)
-        setListing(listingDoc.data())
-      }
-    }
-    fetchListing()
-  }, [firestore, listingId])
-
-  async function updateListing() {
-    const listingRef = doc(collection(firestore, "listings", "forSale", "properties"), listingId)
-    await updateDoc(listingRef, {
-      zip,
-      street,
-      state,
-      price,
-      description,
-      bathrooms,
-      bedrooms,
-      city,
-      images: {"image1":image1, "image2":image2, "image3":image3}
-    });
+  
+  async function fetchListings() {
+    const listingsQuery = query(collection(firestore, "listings", "forSale", "properties"));
+    const listingsSnapshot = await getDocs(listingsQuery);
+    const listingsData = listingsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    setListings(listingsData);
   }
 
-  const handleSubmit = async(event) => {
+  async function editListing(id, updatedListing) {
+    await updateDoc(doc(collection(firestore, "listings", "forSale", "properties"), id), updatedListing);
+    setSelectedListing(null);
+    fetchListings();
+  }
+
+  const handleListingChange = (event) => {
+    const { value } = event.target;
+    const selectedListing = listings.find((listing) => listing.id === value);
+    setSelectedListing(value);
+    setStreet(selectedListing.street);
+    setZip(selectedListing.zip);
+    setStreet(selectedListing.street)
+    setState(selectedListing.state)
+    setCity(selectedListing.city)
+    setPrice(selectedListing.price)
+    setDescription(selectedListing.description)
+    setBathrooms(selectedListing.bathrooms)
+    setBedrooms(selectedListing.bedrooms)
+    setImage1(selectedListing.images.image1)
+    setImage2(selectedListing.images.image2)
+    setImage3(selectedListing.images.image3)
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await updateListing()
-      alert("Listing successfully updated")
+      const updatedListing = { zip, street, state, city, price, description, bathrooms, bedrooms, images:[image1, image2, image3]};
+      await editListing(selectedListing, updatedListing);
+      alert("Listing successfully updated");
     } catch {
-      alert("Error, listing could not be updated")
+      alert("Error, listing could not be updated");
     }
   };
 
-  if (!listing) {
-    return <div>Loading...</div>
-  }
+  useEffect(() => {
+    fetchListings();
+  },[])
 
   return (
     <Box sx={{ width: 750, margin: "0 auto", marginTop: "30px" }}>
       <Typography variant="h5" sx={{ mb: 2, textAlign: "center" }}>Edit Listing</Typography>
       <Box component="form" onSubmit={handleSubmit} sx={{ display: "grid", gap: 2 }}>
-        <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <label htmlFor="listings">Select a listing to edit:</label>
+          <select name="listings" id="listings" value={selectedListing} onChange={handleListingChange}>
+            <option value="none">Select a listing</option>
+            {listings.map((listing) => (
+              <option key={listing.id} value={listing.id}>
+                {listing.street}, {listing.city}, {listing.state} - ${listing.price}
+              </option>
+            ))}
+          </select>
+        </Box>
+        {selectedListing && (
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
           <TextField
             label="Zip"
-            value={zip || listing.zip}
+            value={zip}
             onChange={(event) => setZip(event.target.value)}
             required
             fullWidth
           />
           <TextField
             label="Street"
-            value={street || listing.street}
+            value={street}
             onChange={(event) => setStreet(event.target.value)}
             required
             fullWidth
@@ -82,34 +102,83 @@ const EditListing = ({ listingId }) => {
         <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
           <TextField
             label="City"
-            value={city || listing.city}
+            value={city}
             onChange={(event) => setCity(event.target.value)}
             required
             fullWidth
           />
           <TextField
             label="State"
-            value={state || listing.state}
+            value={state}
             onChange={(event) => setState(event.target.value)}
             required
             fullWidth
-            />
+          />
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+          <TextField
+            label="Price"
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+            required
+            fullWidth
+          />
+          <TextField
+            label="Bathrooms"
+            type="number"
+            value={bathrooms}
+            onChange={(event) => setBathrooms(event.target.value)}
+            required
+            fullWidth
+          />
+          <TextField
+            label="Bedrooms"
+            type="number"
+            value={bedrooms}
+            onChange={(event) => setBedrooms(event.target.value)}
+            required
+            fullWidth
+          />
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+          <TextField
+            label="Image 1"
+            value={image1}
+            onChange={(event) => setImage1(event.target.value)}
+            required
+            fullWidth
+          />
+          <TextField
+            label="Image 2"
+            value={image2}
+            onChange={(event) => setImage2(event.target.value)}
+            required
+            fullWidth
+          />
+          <TextField
+            label="Image 3"
+            value={image3}
+            onChange={(event) => setImage3(event.target.value)}
+            required
+            fullWidth
+          />
         </Box>
         <TextField
           label="Description"
-          value={description || listing.description}
-          onChange={(event) => setDescription(event.target.value)}
           multiline
+          rows={4}
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
           required
           fullWidth
         />
-        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-      <Button variant="contained" onClick="">Cancel</Button>
-    <Button variant="contained" type="submit">Save</Button>
+          </Box>
+        )}
+        <Button variant="contained" type="submit" disabled={!selectedListing} sx={{ justifySelf: "center" }}>Edit Listing</Button>
+      </Box>
+      <br/>
     </Box>
-  </Box>
-</Box>
-);
+  );
 }
 
-export default EditListing
+export default EditListing;
