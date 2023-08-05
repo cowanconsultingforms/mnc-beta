4dbbea0715dad8470c5720d8b69ba9bde46a4afe
devakmmm <139250466+devakmmm@users.noreply.github.com>
2023-08-05 15:04:50 -0400

    Update Footer.js

--- src/components/Misc/Footer.js
+++ src/components/Misc/Footer.js
@@ -18,10 +18,12 @@ export const FooterImage1 = () => {
       <img
         src={imageURL}
         alt="i1"
-        height="250px"
-        width="175px"
-        border="1px"
-        style={{ padding: "10px" }}
+        height="220px"
+        width="320px"
+        
+        style={{ padding: "10px", border: "1px solid #ccc", // Adding a light grey border
+        boxShadow: "0px 0px 10px rgba(255, 255, 255, 0.5)",
+       }}
       ></img>
     </React.Fragment>
   );
@@ -42,10 +44,11 @@ export const FooterImage2 = () => {
       <img
         src={imageURL}
         alt="i2"
-        height="250px"
-        width="175px"
-        border="1px"
-        style={{ padding: "10px" }}
+        height="220px"
+        width="320px"
+      
+        style={{ padding: "10px", boxShadow: "0px 0px 10px rgba(255, 255, 255, 0.5)" ,border: "1px solid #ccc" // Adding a light grey border
+      }}
       />
     </React.Fragment>
   );
@@ -65,10 +68,11 @@ export const FooterImage3 = () => {
       <img
         src={imageURL}
         alt="i2"
-        height="250px"
-        width="175px"
-        border="1px"
-        style={{ padding: "10px" }}
+        height="220px"
+        width="320px"
+        
+        style={{ padding: "10px", border: "1px solid #ccc", // Adding a light grey border
+        boxShadow: "0px 0px 10px rgba(255, 255, 255, 0.5)" }}
       />
     </React.Fragment>
   );
@@ -76,93 +80,43 @@ export const FooterImage3 = () => {
 export const Footer = () => {
   return (
     <Box
-      component="div"
+      component="footer"
       sx={{
-        display: "flex",
-        alignItems: "bottom",
-        justifyContent: "center",
-        flexDirection: "column",
+        backgroundColor: "#fff", // Dark background color
+        color: "##333", // White text color
+        padding: "40px 0",
+        textAlign: "center",
+        fontSize: "10.0pt",
+        lineHeight: "1.6",
+        
       }}
     >
-      <Grid
-        spacing={2}
-        container
-        component="div"
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-        }}
-        className="home-footer"
-      >
-        <Grid item sx={{ alignItems: "center" }}>
-          <FooterImage1 />
-        </Grid>
-        <Divider />
-        <Grid item>
-          <FooterImage3 />{" "}
-        </Grid>
-        <Grid item>
-          <FooterImage2 />{" "}
-        </Grid>
-      </Grid>
-      <Divider />{" "}
+     <FooterImagesRow />
+      <Divider sx={{ margin: "20px 0", borderColor: "#555" }} />
       <Box
         component="span"
-        className="footer-bottom"
         sx={{
-          margin: "0in",
-          marginBottom: ".0001pt",
-          textAlign: "center",
-          fontSize: "10.0pt",
-          lineHeight: "2",
-          marginTop: "3px",
-          alignItems: "center",
-          justifyContent: "center",
+          marginBottom: "10px",
         }}
       >
-        Copyright © MNC Development, Inc. 2008-present. All rights reserved.
+        © MNC Development, Inc. 2008-present. All rights reserved.
       </Box>
       <Box
         component="span"
         sx={{
-          margin: "0in",
-          marginBottom: ".0001pt",
-          textAlign: "center",
-          fontSize: "10.0pt",
-          lineHeight: "2",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      ></Box>
-      <Box
-        component="span"
-        sx={{
-          margin: "0in",
-          marginBottom: ".0001pt",
-          textAlign: "center",
-          fontSize: "10.0pt",
-          lineHeight: "2",
-          alignItems: "center",
-          justifyContent: "center",
+          marginBottom: "10px" ,
         }}
       >
-        31 Buffalo Avenue, Brooklyn, New York 11233|Phone:1-718-771-5811 or
-        1-877-732-3492|Fax: 1-877-760-2763 or 1-718-771-5900{" "}
-        <Link to={"/contact"}> info@mncdevelopment.com</Link>
+        31 Buffalo Avenue, Brooklyn, New York 11233 | Phone: 1-718-771-5811 or
+        1-877-732-3492 | Fax: 1-877-760-2763 or 1-718-771-5900{" "}
+        <Link to={"/contact"} style={{ color: "#333" }}>
+          info@mncdevelopment.com  
+        </Link>
       </Box>
       <Box
         component="span"
         sx={{
-          margin: "0in",
-          marginBottom: ".0001pt",
-          textAlign: "center",
-          fontSize: "10.0pt",
-          lineHeight: "2",
-          alignItems: "center",
-          justifyContent: "center",
+          marginBottom: "10px",
         }}
       >
         MNC Development and the MNC Development logos are trademarks of MNC
@@ -171,40 +125,48 @@ export const Footer = () => {
       <Box
         component="span"
         sx={{
-          margin: "0in",
-          marginBottom: ".0001pt",
-          textAlign: "center",
-          fontSize: "10.0pt",
-          lineHeight: "2",
-          alignItems: "center",
-          justifyContent: "center",
+          marginBottom: "10px", 
+          marginright: "10px",
         }}
       >
         MNC Development, Inc. as a NYS licensed Real Estate Broker fully
         supports the principles of the Fair Housing Act and the Equal
         Opportunity Act. Listing information is deemed reliable, but is not
-        guaranteed
+        guaranteed.
       </Box>
     </Box>
   );
 };
+
+const FooterImagesRow = () => {
+  return (
+    <Grid
+      container
+      component="div"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "20px",
+        marginTop: "20px",
+      }}
+    >
+      <Grid item>
+        <FooterImage1 />
+      </Grid>
+      
+     
+      <Grid item>
+        <FooterImage3 />
+      </Grid>
+      <Grid item>
+        <FooterImage2 />
+      </Grid>
+    </Grid>
+  );
+};
+
 export default Footer;
-
-/* Breif: Defines a footer component for a web page using React and Firebase storage. 
-It also uses Material-UI for UI components. 
-The footer contains three images, which are loaded using useStorageDownloadURL hook from Firebase storage.
-
-The FooterImage1, FooterImage2, and FooterImage3 components define an image each by calling the useStorageDownloadURL hook with the 
-reference to the Firebase storage location of the image. 
-If the status of the hook is "loading", the component displays the text "loading...". 
-If the status is not "loading", the component displays the image with the URL obtained from the hook.
-
-The Footer component defines the footer itself, which contains the three images from FooterImage1, FooterImage2, and FooterImage3 
-components arranged in a row using a Grid component from Material-UI. The footer also contains text and links to contact information and disclaimers.
-
-Responsive footer with images that are loaded dynamically from Firebase storage, allowing for easy maintenance of the images without needing to
-update the web page.*/
-
 
 
 
