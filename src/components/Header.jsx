b69eadeb13a6203c7087bfbf43a1f579230cbe8f
W-McPhail <william.m.mcphail@gmail.com>
2023-08-06 19:55:27 -0400

    mobile menu dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import MncLogo from "../assets/svg/mnc-logo.svg";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import MncLogo from "../assets/svg/mnc-logo.svg";
+
 const Header = () => {
   const [pageState, setPageState] = useState("Sign in");
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
   const auth = getAuth();
@@ -24,6 +26,12 @@ const Header = () => {
     if (route === location.pathname) {
       return true;
     }
+    return false;
+  };
+
+  // Toggle mobile menu
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
@@ -39,7 +47,32 @@ const Header = () => {
             onClick={() => navigate("/")}
           />
         </div>
-        <div>
+
+        {/* Mobile Menu */}
+        <div className="lg:hidden">
+          <button
+            className="flex items-center px-3 py-2  rounded text-gray-500 hover:text-black"
+            onClick={toggleMobileMenu}
+          >
+            <svg
+              className="w-4 h-4 fill-current"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 0h20v20H0z" fill="none" />
+              <path
+                d="M2.5 6h15M2.5 10h15M2.5 14h15"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Desktop Menu */}
+        <div className="hidden lg:block">
           <ul className="flex space-x-10">
             {/* Home button */}
             <li
@@ -105,6 +138,90 @@ const Header = () => {
           </ul>
         </div>
       </header>
+
+      {/* Mobile Dropdown Menu */}
+      {isMobileMenuOpen && (
+        <div className="lg:hidden">
+          <ul className="flex flex-col space-y-2 ml-8">
+            {/* Home button */}
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold  border-b-[3px] 
+              ${!pathMatchRoute("/") && "text-gray-400 border-b-transparent"} 
+              ${pathMatchRoute("/") && "text-black border-b-gray-900 "}`}
+              onClick={() => {
+                navigate("/");
+                toggleMobileMenu();
+              }}
+            >
+              Home
+            </li>
+
+            {/* Contact button */}
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
+                !pathMatchRoute("/map") && "text-gray-400 border-b-transparent"
+              } ${pathMatchRoute("/map") && "text-black border-b-gray-900"}`}
+              onClick={() => {
+                navigate("/map");
+                toggleMobileMenu();
+              }}
+            >
+              Map
+            </li>
+
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
+                !pathMatchRoute("/contact-us") &&
+                "text-gray-400 border-b-transparent"
+              } ${
+                pathMatchRoute("/contact-us") && "text-black border-b-gray-900"
+              }`}
+              onClick={() => {
+                navigate("/contact-us");
+                toggleMobileMenu();
+              }}
+            >
+              Contact Us
+            </li>
+
+            {/* Offers button */}
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px]
+              ${
+                !pathMatchRoute("/offers") &&
+                "text-gray-400 border-b-transparent"
+              } 
+              ${pathMatchRoute("/offers") && "text-black border-b-gray-900"}`}
+              onClick={() => {
+                navigate("/offers");
+                toggleMobileMenu();
+              }}
+            >
+              Offers
+            </li>
+
+            {/* Sign in button */}
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px]
+              ${
+                !pathMatchRoute("/sign-in") &&
+                !pathMatchRoute("/profile") &&
+                "text-gray-400 border-b-transparent"
+              } 
+              ${
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                "text-black border-b-gray-900"
+              }`}
+              onClick={() => {
+                navigate("/profile");
+                toggleMobileMenu();
+              }}
+            >
+              {pageState}
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
